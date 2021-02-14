16f44259ca3bbfda98833ac5c6fbdee115175a60
Gijs Nelissen <gijs@prezly.com>
2021-02-14 11:00:29 +0100

    Add paths in tsconfig

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Header} from "../components/Header";
-import {Footer} from "../components/Footer";
+import {Header} from "@/components/Header";
+import {Footer} from "@/components/Footer";
 
 export default class HomePage extends React.Component {
     render() {
