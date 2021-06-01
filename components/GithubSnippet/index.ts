472328645229ee22b43040654170795f92085e98
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-06-01 12:26:33 +0200

    Do not SSR code embeds

--- components/GithubSnippet/index.ts
+++ components/GithubSnippet/index.ts
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic";
 
-const GithubSnippet = dynamic(() => import("./GithubSnippet"));
+const GithubSnippet = dynamic(() => import("./GithubSnippet"), { ssr: false });
 
 export default GithubSnippet;
