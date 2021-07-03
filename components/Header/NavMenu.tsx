9ebd8466818ad8363ed687f15fb182fefc5ee952
Gijs Nelissen <gijs@prezly.com>
2021-07-03 15:19:42 +0200

    Fix problem with upgrade

--- components/Header/NavMenu.tsx
+++ components/Header/NavMenu.tsx
@@ -1,12 +1,28 @@
-import { chakra, HTMLChakraProps, useColorModeValue as mode } from '@chakra-ui/react'
-import { HTMLMotionProps, isValidMotionProp, motion, Variants } from 'framer-motion'
+import { useColorModeValue } from '@chakra-ui/react'
+import { Variants } from 'framer-motion'
 import * as React from 'react'
+import { MotionBox, MotionBoxProps } from './MotionBox'
 
-const filter = (props: any) => Object.fromEntries(Object.entries(props).filter(([key]) => !isValidMotionProp(key)))
+export const NavMenu = React.forwardRef<HTMLDivElement, MotionBoxProps>((props, ref) => (
+    <MotionBox
+        ref={ref}
+        initial="init"
+        variants={variants}
+        outline="0"
+        opacity="0"
+        bg={useColorModeValue('white', 'gray.700')}
+        w="full"
+        shadow="lg"
+        px="4"
+        pos="absolute"
+        insetX="0"
+        pt="6"
+        pb="12"
+        {...props}
+    />
+))
 
-type DivProps = Omit<HTMLChakraProps<'div'>, keyof HTMLMotionProps<'div'>>
-
-export const MotionDiv = motion.custom(React.forwardRef((props: DivProps, ref: React.Ref<any>) => <chakra.div ref={ref} {...filter(props)} />))
+NavMenu.displayName = 'NavMenu'
 
 const variants: Variants = {
     init: {
@@ -30,24 +46,3 @@ const variants: Variants = {
         },
     },
 }
-
-type MotionDivProps = DivProps & HTMLMotionProps<'div'>
-
-export const NavMenu = React.forwardRef((props: MotionDivProps, ref: React.Ref<any>) => (
-    <MotionDiv
-        ref={ref}
-        initial="init"
-        variants={variants}
-        outline="0"
-        opacity="0"
-        bg={mode('white', 'gray.700')}
-        w="full"
-        shadow="lg"
-        px="4"
-        pos="absolute"
-        insetX="0"
-        pt="6"
-        pb="12"
-        {...props}
-    />
-))
